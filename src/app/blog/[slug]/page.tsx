b9b2e45c2f0f9bdf768038a277f82314ccfe52df
Alwurts <jota780@gmail.com>
2024-10-24 14:22:08 +0200

    Change to next js 14

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { notFound } from "next/navigation";
 import fs from "node:fs";
 import path from "node:path";
 
@@ -12,6 +13,9 @@ import path from "node:path";
 
 export default async function Page({ params }: { params: { slug: string } }) {
 	const filePath = path.join(process.cwd(), `src/posts/${params.slug}.mdx`);
+	if (!fs.existsSync(filePath)) {
+		return notFound();
+	}
 	const source = fs.readFileSync(filePath, "utf8");
 	return <MDXRemote source={source} />;
 }
--- src/app/blog/page.tsx
+++
@@ -1,5 +0,0 @@
-import Post from "@/posts/24-10-24.mdx";
-
-export default function Page() {
-	return <Post />;
-}
