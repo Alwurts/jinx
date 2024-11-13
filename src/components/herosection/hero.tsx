dec558fb4881e219ac4be99353b086f9ab862581
Ilse Löhr <ilse.loehr@s2023.tu-chemnitz.de>
2024-11-13 21:02:37 +0100

    width of hero section is aligned to divider

--- src/components/herosection/hero.tsx
+++ src/components/herosection/hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HeroSection() {
     return (
         <section className="py-10">
-            <div className="grid max-w-screen-xl py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-24 lg:grid-cols-12">
+            <div className="grid py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-24 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                         AI-Driven BPMN for Smarter Business
@@ -11,7 +11,7 @@ export default function HeroSection() {
                     <p className="max-w-2xl lg:max-w-lg mb-6 font-light text-secondary-foreground lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                         We are the partner of your future transforming legacy business processes into efficient, AI-enhanced automated workflows powered by BPMN.
                     </p>
-                    <div className="flex space-x-3">
+                    <div className="flex space-y-6 flex-col md:space-y-0 md:flex-row md:space-x-3">
                         <Link
 									href="/"
 									className="inline-flex items-center justify-center px-5 py-3 text-primary-foreground text-base font-semibold rounded-xl bg-primary hover:bg-primary/80 focus:ring-2 focus:ring-primary dark:hover:bg-primary/80"
@@ -26,7 +26,7 @@ export default function HeroSection() {
                         </Link>
                     </div>
                 </div>
-                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex lg:flex-col">
                     <img
                         src="/computer.png"
                         alt="mockup"
