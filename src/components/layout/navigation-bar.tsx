988f22a89991bbb7f476e52d5943be5846274111
Amir ali <amiralikhamseh01@gmail.com>
2024-11-14 19:02:42 +0100

    feature : Contact us page added

--- src/components/layout/navigation-bar.tsx
+++ src/components/layout/navigation-bar.tsx
@@ -7,42 +7,42 @@ import { ThemeToggle } from "../theme/theme-toggle";
 import LogoIconMystical from "@/components/icons/logo-icon-mystical";
 
 export default function Navbar() {
-  const pathname = usePathname();
-  const navigationLinks = [
-    {
-      href: "/",
-      label: "Home",
-    },
-    {
-      href: "/blog",
-      label: "Blog",
-    },
-    {
-      href: "/contact",
-      label: "Contact",
-    },
-  ];
-  return (
-    <nav className="p-4 bg-card">
-      <div className="container mx-auto flex justify-between items-center">
-        <ul className="flex items-center justify-center space-x-4">
-          <LogoIconMystical className="h-8 w-full text-foreground" />
-          {navigationLinks.map((link) => (
-            <li key={link.href}>
-              <Link
-                href={link.href}
-                className={cn(
-                  "text-lg hover:underline ",
-                  pathname === link.href && "font-bold underline"
-                )}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ThemeToggle />
-      </div>
-    </nav>
-  );
+	const pathname = usePathname();
+	const navigationLinks = [
+		{
+			href: "/",
+			label: "Home",
+		},
+		{
+			href: "/blog",
+			label: "Blog",
+		},
+		{
+			href: "/contact",
+			label: "Contact",
+		},
+	];
+	return (
+		<nav className="p-4 bg-card">
+			<div className="container mx-auto flex justify-between items-center">
+				<ul className="flex items-center justify-center space-x-4">
+					<LogoIconMystical className="h-8 w-full text-foreground" />
+					{navigationLinks.map((link) => (
+						<li key={link.href}>
+							<Link
+								href={link.href}
+								className={cn(
+									"text-lg hover:underline ",
+									pathname === link.href && "font-bold underline",
+								)}
+							>
+								{link.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<ThemeToggle />
+			</div>
+		</nav>
+	);
 }
