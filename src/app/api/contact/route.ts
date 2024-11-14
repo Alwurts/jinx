4c8c6c5340f5ca70b630267ff187b02e897867cd
Amir ali <amiralikhamseh01@gmail.com>
2024-11-14 20:34:00 +0100

    refactor: the API route added for submitting teh contact form

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,12 +5,12 @@ Method : POST
 Path: /api/contact
 */
 export async function POST(req: Request) {
-  try {
-    const data = await req.json();
-    console.log("Data received from frontend:", data);
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-    return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
-  }
+	try {
+		const data = await req.json();
+		console.log("Data received from frontend:", data);
+		return NextResponse.json(data);
+	} catch (error) {
+		console.error("Error parsing JSON:", error);
+		return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+	}
 }
