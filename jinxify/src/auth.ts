c0922277d9cf24ff9f1d628ad36d782ff1547f95
Ilse Löhr <ilse.loehr@s2023.tu-chemnitz.de>
2024-11-28 15:49:45 +0100

    feature: created directory and diagram table for db

--- jinxify/src/auth.ts
+++ jinxify/src/auth.ts
@@ -4,6 +4,7 @@ import db from "./server/db";
 import { sessions, users, verificationTokens } from "./server/db/schema/auth";
 import { accounts } from "./server/db/schema/auth";
 import Github from "next-auth/providers/github";
+import { directory } from "./server/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: DrizzleAdapter(db, {
@@ -12,6 +13,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 		sessionsTable: sessions,
 		verificationTokensTable: verificationTokens,
 	}),
-
+	events: {
+		async createUser({ user }) {
+			console.log("createUser event triggered");
+			try {
+				const userId = user.id;
+				console.log("User ID:", userId);
+				if (!userId) {
+					throw new Error("User ID not found on createUser event");
+				}
+				await db.insert(directory).values({
+					title: "Home",
+					userId,
+					directoryId: null,
+				});
+			} catch (error) {
+				console.error("Error initializing user data:", error);
+			}
+		},
+	},
 	providers: [Github],
 });
