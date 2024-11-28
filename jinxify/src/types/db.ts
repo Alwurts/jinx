c315142872fab80cd5cac3693e42731674d2858e
Alwurts <jota780@gmail.com>
2024-11-28 16:29:27 +0100

    Fix: RUn lint

--- jinxify/src/types/db.ts
+++ jinxify/src/types/db.ts
@@ -1,8 +1,12 @@
 import type { InferQueryModel } from "./infer-database";
 
 export type TDiagram = InferQueryModel<"diagram">;
-export type TDirectory = InferQueryModel<"directory", undefined, {
-    parent: true,
-    directories: true,
-    diagrams: true,
-}>;
+export type TDirectory = InferQueryModel<
+	"directory",
+	undefined,
+	{
+		parent: true;
+		directories: true;
+		diagrams: true;
+	}
+>;
