8542636038c572c21fe96e53e412f86b4f83c2ad
Ilse Löhr <ilse.loehr@s2023.tu-chemnitz.de>
2024-11-29 15:01:27 +0100

    add image to auth page

--- jinxify/src/app/page.tsx
+++ jinxify/src/app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link"
 
 import { UserAuthForm } from "../components/auth/user-auth-form"
 import Icon from "../components/icons/logo-icon";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -11,41 +13,34 @@ export const metadata: Metadata = {
 }
 
 export default function Home() {
-	return (	
-	 <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        {/* <Link
+  return (
+    <>
+      <div className="container relative h-screen flex-col items-center justify-center p-4 md:p-0 grid max-w-full lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="absolute left-6 top-8">
+			<Icon className="h-12 w-12 mr-2" />
+		</div>
+		<Link
           href="/examples/authentication"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            "absolute right-6 top-10 bg-secondary"
           )}
         >
-          Login // add Dark or LightMode Button
-        </Link> */}
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-			<Icon className={"text-white dark:text-primary h-6 w-6 mr-2"} />
-			<span>jinx</span>
+          Login
+        </Link> 
+        <div className="relative hidden h-full flex-col bg-muted p-10 dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-primary opacity-85 z-10" />
+		  	<Image
+				src="/images/business_portal_3d_graphic.png"
+				width={1280}
+				height={843}
+				alt="Business process 3d graphic"
+				className="absolute inset-0 w-full h-full object-cover z-0"
+			/>
+          <div className="relative z-20 flex items-center text-lg font-medium text-secondary">
+			<Icon className={"dark:text-primary h-12 w-12 mr-2"} />
           </div>
-          <div className="relative z-20 mt-auto">
+          <div className="relative z-20 mt-auto text-secondary">
             <blockquote className="space-y-2 italic">
               <p className="text-lg">
                 &ldquo;jinxify has streamlined our processes with AI-driven to-dos and workflow automation. 
@@ -59,11 +54,11 @@ export default function Home() {
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Start Your jinxify Portal
+              <h1 className="text-4xl font-semibold tracking-tight">
+                Enter the jinxify Portal
               </h1>
               <p className="text-sm text-muted-foreground">
-                Sign up using Your GitHub credentials
+                Sign up seamlessly with your GitHub credentials
               </p>
             </div>
             <UserAuthForm />
@@ -88,5 +83,5 @@ export default function Home() {
         </div>
       </div>
     </>
-	);
+  );
 }
