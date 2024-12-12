281f49091c70e80008a13b9636e0ec7fc1526d47
LepuriK <ketjonalepuri00@gmail.com>
2024-12-12 10:40:04 +0100

    feature/design of sidebar

--- jinxify/src/components/layout/sidebar.tsx
+++ jinxify/src/components/layout/sidebar.tsx
@@ -1,14 +1,13 @@
 "use client";
-import {
-	Calendar,
-	Folder,
-	Heart,
-	Home,
-	Inbox,
-	Search,
-	Settings,
-} from "lucide-react";
+import { Folder, Heart, Home, Search } from "lucide-react";
+import { TbHelpSquare } from "react-icons/tb";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { GoPeople } from "react-icons/go";
+import { VscAccount } from "react-icons/vsc";
 import { GrTask } from "react-icons/gr";
+import { FiEdit } from "react-icons/fi";
+import { CgLogOut } from "react-icons/cg";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import {
@@ -22,45 +21,34 @@ import {
 	SidebarMenu,
 	SidebarMenuButton,
 	SidebarMenuItem,
-	SidebarRail,
 	SidebarSeparator,
 } from "@/components/ui/sidebar";
+
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuLabel,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "../ui/dialog";
-import { Button } from "../ui/button";
+
 import type { Session } from "next-auth";
 import { useEffect, useState } from "react";
 import { signOut } from "next-auth/react";
-import {
-	EditProfile,
-	type User,
-} from "@/app/(protected-app)/dashboard/editProfile";
 import { toast } from "react-toastify";
 import { useQuery } from "@tanstack/react-query";
+import Logo from "../icons/logo-icon";
+import Link from "next/link";
 
 // Menu items.
-const items = [
+const applicationItems = [
 	{
 		title: "Home",
 		url: "/dashboard",
 		icon: Home,
 	},
 	{
-		title: "Recent files",
+		title: "Recent Files",
 		url: "#",
 		icon: Folder,
 	},
@@ -70,16 +58,35 @@ const items = [
 		icon: GrTask,
 	},
 	{
-		title: "Search",
-		url: "#",
-		icon: Search,
-	},
-	{
 		title: "Favorites",
 		url: "#",
 		icon: Heart,
 	},
+	{
+		title: "Search",
+		url: "#",
+		icon: Search,
+	},
 ];
+
+const settingsItems = [
+	{
+		title: "Preferences",
+		url: "https://jinx-team.vercel.app",
+		icon: GiSettingsKnobs,
+	},
+	{
+		title: "Help",
+		url: "https://jinx-team.vercel.app/contact",
+		icon: TbHelpSquare,
+	},
+	{
+		title: "About jinx",
+		url: "https://jinx-team.vercel.app/about",
+		icon: GoPeople,
+	},
+];
+
 type Props = {
 	session: Session | null;
 };
@@ -125,38 +132,64 @@ export function AppSidebar({ session }: Props) {
 			});
 		}
 	}, [userError]);
-
 	return (
-		<Sidebar className=" h-screen flex flex-col">
-			<SidebarHeader className="p-4 flex items-center justify-between">
-				<div className="flex items-center space-x-2">
-					<img
-						src="/images/logo_icon_single.svg"
-						alt="Logo"
-						className="w-6 h-6"
-					/>
-					<h1 className="text-lg font-semibold">jinxify</h1>
+		<Sidebar className="h-screen rounded-tr-lg flex flex-col">
+			<SidebarHeader className="p-9 bg-[url('/images/diamond.png')] bg-cover bg-top rounded-tr-lg">
+				<div className="flex items-center space-x-3">
+					<Logo className="w-10 h-10 brightness-0 invert" />
+					<h1 className="text-xl font-semibold text-white">jinxify Portal</h1>
 				</div>
 			</SidebarHeader>
 
 			<SidebarSeparator />
 
-			<SidebarContent className="flex-1 overflow-y-auto">
+			<SidebarContent className="flex-1">
 				<SidebarGroup>
-					<SidebarGroupLabel className="text-gray-400 text-xs uppercase px-4 py-2">
+					<SidebarGroupLabel className="text-gray-400 text-sm px-4 py-2 mt-2 mb-2 font-semibold">
 						Application
 					</SidebarGroupLabel>
 					<SidebarGroupContent>
 						<SidebarMenu>
-							{items.map((item) => (
+							{applicationItems.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<Link
+											href={item.url}
+											className="group/item  flex items-center space-x-3 px-4 py-2 font-semibold text-gray-700 hover:bg-primary/5 cursor-pointer w-full rounded-lg transition-all duration-200"
+										>
+											<span className="text-gray-700 group-hover/item:text-purple-700">
+												<item.icon className="w-5 h-5" />
+											</span>
+											<span className="group-hover/item:text-purple-700">
+												{item.title}
+											</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+
+				<SidebarGroup className="mt-20">
+					<SidebarGroupLabel className="text-gray-400 text-sm px-4 py-2 mb-2 font-semibold">
+						Settings
+					</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{settingsItems.map((item) => (
 								<SidebarMenuItem key={item.title}>
 									<SidebarMenuButton asChild>
 										<a
 											href={item.url}
-											className="flex items-center space-x-2 p-2 rounded-md"
+											className="group/item  flex items-center space-x-3 px-4 py-2 font-semibold text-gray-700 hover:bg-primary/5 cursor-pointer w-full rounded-lg"
 										>
-											{item.icon && <item.icon />}
-											<span>{item.title}</span>
+											<div className="text-gray-700 group-hover/item:text-purple-700">
+												<item.icon className="w-5 h-5" />
+											</div>
+											<span className="group-hover/item:text-purple-700">
+												{item.title}
+											</span>
 										</a>
 									</SidebarMenuButton>
 								</SidebarMenuItem>
@@ -165,71 +198,68 @@ export function AppSidebar({ session }: Props) {
 					</SidebarGroupContent>
 				</SidebarGroup>
 			</SidebarContent>
-
-			<SidebarSeparator />
-
-			<SidebarFooter className=" p-4 flex flex-col items-center">
-				<Avatar>
-					<AvatarImage
-						src={session?.user?.image ?? "/images/jinx.png"}
-						alt="User Avatar"
-						className="w-20 h-15 rounded-full"
-					/>
-					<AvatarFallback>User</AvatarFallback>
-				</Avatar>
-				<div className="mt-2 text-center">
-					<p className="text-sm font-medium">
-						{isLoading ? "Loading..." : `${userData?.user?.name}'s workspace`}
-					</p>
-				</div>
+			<div className="py-3 px-3">
+				<SidebarSeparator />
+			</div>
+			<SidebarFooter className="p-6 mb-4 ">
+				<SidebarGroupLabel className="text-gray-400 text-sm px-4 py-5 font-semibold">
+					Profile
+				</SidebarGroupLabel>
 				<DropdownMenu>
-					<DropdownMenuTrigger className="mt-2 text-sm w-full " asChild>
-						<Button
-							variant="outline"
-							className="px-2 py-1 text-xs rounded-md w-full"
-						>
-							User Profile
-						</Button>
+					<DropdownMenuTrigger className="w-full rounded-lg focus:outline-none">
+						<div className="flex items-center space-x-3 p-4 mb-3 rounded-md bg-gray-50 border border-gray-200 shadow-md hover:bg-purple-50 cursor-pointer transition-all duration-200 hover:shadow-md">
+							<Avatar>
+								<AvatarImage
+									src={session?.user?.image ?? "/images/jinx.png"}
+									alt="User Avatar"
+								/>
+								<AvatarFallback>User</AvatarFallback>
+							</Avatar>
+							<div className="flex-1 text-left">
+								<p className="text-sm font-medium">
+									{userData?.user?.name || "User"}
+								</p>
+								<p className="text-xs text-gray-500">My Workspace</p>
+							</div>
+						</div>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent
-						className="bg-white text-black border border-gray-200 shadow-lg rounded-md ml- 4 mb-14"
+						className="w-56 mb-12 bg-gray-50"
 						side="right"
 						align="start"
+						alignOffset={-70}
+						sideOffset={10}
 					>
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<div className="flex items-center space-x-3 p-4 mb-1  rounded-md bg-[url('/images/diamond.png')] bg-cover bg-center border border-gray-50 shadow-sm hover:bg-purple-50 cursor-pointer transition-all duration-200 hover:shadow-md">
+							<div className="flex-1 text-center">
+								<p className="text-sm font-medium text-white">User Profile</p>
+							</div>
+						</div>
+
+						<DropdownMenuItem className="flex items-center space-x-2 px-3 py-2 ">
+							<VscAccount className="w-4 h-4" />
+							<span>Account</span>
+						</DropdownMenuItem>
 						<DropdownMenuSeparator />
 
-						<EditProfile />
+						<div className="py-1">
+							<DropdownMenuItem className="flex items-center space-x-2 px-3 py-2 ">
+								<FiEdit className="w-4 h-4" />
+								<span>Edit Profile</span>
+							</DropdownMenuItem>
 
-						<DropdownMenuItem>
-							<Dialog>
-								<DialogTrigger className="w-full text-left">
-									Delete Account
-								</DialogTrigger>
-								<DialogContent>
-									<DialogHeader>
-										<DialogTitle>Are you absolutely sure?</DialogTitle>
-										<DialogDescription>
-											This action cannot be undone. This will permanently delete
-											your account and remove your data from our servers.
-										</DialogDescription>
-									</DialogHeader>
-								</DialogContent>
-							</Dialog>
-						</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem
+								className="flex items-center space-x-2 px-3 py-2 "
+								onClick={handleLogout}
+							>
+								<CgLogOut className="w-4 h-4 space-x-2" />
+								<span>{isLoading ? "Logging out..." : "Log out"}</span>
+							</DropdownMenuItem>
+						</div>
 					</DropdownMenuContent>
-					<Button
-						className="px-2 py-1 text-xs rounded-md w-full"
-						onClick={handleLogout}
-						disabled={isLoading}
-						variant="destructive"
-					>
-						{isLoading ? "Logging out..." : "Logout"}
-					</Button>
 				</DropdownMenu>
 			</SidebarFooter>
-
-			<SidebarRail />
 		</Sidebar>
 	);
 }
