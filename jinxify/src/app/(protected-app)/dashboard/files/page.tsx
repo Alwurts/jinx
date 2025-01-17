59c6e78638a4e55f578868aad58bdcfff321a1d5
XhesSamarxhiu <xhesikasaamrxhiu21@gmail.com>
2025-01-17 17:09:13 +0000

    feature:filter options

--- jinxify/src/app/(protected-app)/dashboard/files/page.tsx
+++ jinxify/src/app/(protected-app)/dashboard/files/page.tsx
@@ -7,209 +7,322 @@ import { Plus, ArrowLeft, FilesIcon } from "lucide-react";
 import type { TDirectory } from "@/types/db";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import {
-	FaFileAlt,
-	FaFolder,
-	FaProjectDiagram,
-	FaFileWord,
+  FaFileAlt,
+  FaFolder,
+  FaProjectDiagram,
+  FaFileWord,
 } from "react-icons/fa";
 
 import { useSession } from "next-auth/react";
 import { DashboardSkeleton } from "@/components/dashboard/file-skeleton";
-import { DirectoryTable } from "@/components/dashboard/directory-table";
-import { DiagramTable } from "@/components/dashboard/files-table";
+
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-	DropdownMenuSeparator,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import ImageOverlayHeader from "@/components/layout/image-overlay-header";
 import { MdMyLocation } from "react-icons/md";
-import { IoIosArrowForward } from "react-icons/io";
+import { LuLayoutGrid } from "react-icons/lu";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useMemo, useState } from "react";
+import { FilesLayout } from "@/components/dashboard/files-layout";
 
 export default function Dashboard() {
-	const queryClient = useQueryClient();
-	const searchParams = useSearchParams();
-	const directoryId = searchParams.get("directoryId") ?? "root";
-	const { data: session, status } = useSession();
-	const router = useRouter();
+  const [viewType, setViewType] = useState<"grid" | "list">("grid");
+  const [selectItem, setSelectItem] = useState<
+    "directory" | "diagram" | "form" | "document" | null
+  >(null);
+  const queryClient = useQueryClient();
+  const searchParams = useSearchParams();
+  const directoryUrlId = searchParams.get("directoryId") ?? "root";
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-	const { data: currentDirectory, isLoading } = useQuery({
-		queryKey: ["directory", directoryId],
-		queryFn: async () => {
-			const response = await fetch(`/api/workspace/${directoryId}`);
-			if (!response.ok) {
-				throw new Error("Failed to fetch directory");
-			}
-			const data = (await response.json()) as TDirectory;
-			return data;
-		},
-	});
+  const { data: currentDirectoryData, isLoading } = useQuery({
+    queryKey: ["directory", directoryUrlId],
+    queryFn: async () => {
+      const response = await fetch(`/api/workspace/${directoryUrlId}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch directory");
+      }
+      const data = (await response.json()) as TDirectory;
+      return data;
+    },
+  });
 
-	const createWorkspaceItem = useMutation({
-		mutationFn: async ({
-			type,
-			title,
-		}: {
-			type: "directory" | "diagram" | "form" | "document";
-			title: string;
-		}) => {
-			const response = await fetch("/api/workspace", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					type,
-					title,
-					directoryId: currentDirectory?.id,
-				}),
-			});
+  const filteredFiles = useMemo(() => {
+	if (!currentDirectoryData) {
+		return
+	}
+    let fileItems = [
+		...currentDirectoryData.directories,
+		...currentDirectoryData.documents,
+		...currentDirectoryData.diagrams,
+		...currentDirectoryData.forms
+	];
 
-			if (!response.ok) {
-				throw new Error("Failed to create workspace item");
-			}
-
-			return response.json();
-		},
-		onSuccess: () => {
-			// Invalidate and refetch the current directory data
-			queryClient.invalidateQueries({ queryKey: ["directory", directoryId] });
-		},
-	});
-
-	const handleCreateFolder = (
-		type: "diagram" | "directory" | "form" | "document",
-	) => {
-		createWorkspaceItem.mutate({
-			type,
-			title: `New ${type === "directory" ? "Folder" : type === "diagram" ? "Diagram" : type === "form" ? "Form" : "Document"}`,
-		});
-	};
-
-	const handleGoBack = () => {
-		if (currentDirectory?.parent?.directoryId) {
-			const newParams = new URLSearchParams();
-			newParams.set("directoryId", currentDirectory.parent.id);
-			router.push(`/dashboard/files?${newParams.toString()}`);
-		} else {
-			router.push("/dashboard/files");
-		}
-	};
-
-	if (isLoading) {
-		return <DashboardSkeleton />;
+	if (selectItem) {
+		fileItems = fileItems.filter((item) => {
+			return item.type === selectItem
+		})
 	}
 
-	return (
-		<>
-			<ImageOverlayHeader
-				title="Files"
-				icon={<FilesIcon className="size-8 text-primary-foreground z-20" />}
-				leftToolbar={
-					<>
-						{directoryId !== "root" && (
-							<Button
-								variant="secondary"
-								size="icon"
-								onClick={handleGoBack}
-								className="hover:bg-gray-100 dark:hover:bg-mystical/30 z-10"
-							>
-								<ArrowLeft className="w-5 h-5" />
-							</Button>
-						)}
-						<SidebarTrigger className="md:hidden z-10 bg-secondary" />
-					</>
-				}
-				rightToolbar={
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button
-								variant="secondary"
-								className="flex items-center gap-2 dark:bg-accent dark:text-secondary-foreground"
-								disabled={createWorkspaceItem.isPending}
-							>
-								New Item
-								<Plus className="w-5 h-5" />
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent>
-							<DropdownMenuItem
-								onClick={() => handleCreateFolder("directory")}
-								className="z-30"
-							>
-								<FaFolder className="w-4 h-4 mr-2" />
-								New Folder
-							</DropdownMenuItem>
-							{directoryId !== "root" && (
-								<>
-									<DropdownMenuSeparator />
-									<DropdownMenuItem
-										onClick={() => handleCreateFolder("diagram")}
-									>
-										<FaProjectDiagram className="w-4 h-4 mr-2" />
-										New Diagram
-									</DropdownMenuItem>
-								</>
-							)}
-							{directoryId !== "root" && (
-								<>
-									<DropdownMenuSeparator />
-									<DropdownMenuItem onClick={() => handleCreateFolder("form")}>
-										<FaFileAlt className="w-4 h-4 mr-2" />
-										New Form
-									</DropdownMenuItem>
-								</>
-							)}
-							{directoryId !== "root" && (
-								<>
-									<DropdownMenuSeparator />
-									<DropdownMenuItem
-										onClick={() => handleCreateFolder("document")}
-									>
-										<FaFileWord className="w-4 h-4 mr-2" />
-										New Document
-									</DropdownMenuItem>
-								</>
-							)}
-						</DropdownMenuContent>
-					</DropdownMenu>
-				}
-			/>
+	if (directoryUrlId === "root") {
+		fileItems = fileItems.filter((item) => {
+			return item.type !== "directory"
+		})
+	}
 
-			<div className="flex items-center gap-4 py-4 px-4 pb-8">
-				<div className="flex items-center space-x-1 text-muted-foreground">
-					<IoIosArrowForward className="w-4 h-4" />
-					<h1 className="text-sm">
-						{currentDirectory?.title || "My Workspace"}
-					</h1>
-				</div>
-			</div>
+	return fileItems
 
-			{currentDirectory?.directories.length === 0 &&
-			currentDirectory?.diagrams.length === 0 &&
-			currentDirectory?.forms.length === 0 &&
-			currentDirectory?.documents.length === 0 ? (
-				<div className="text-center text-gray-500 py-8">
-					<p className="mb-4">
-						No folders or files yet. Create one to get started!
-					</p>
-					<Button onClick={() => handleCreateFolder("directory")}>
-						<Plus className="w-4 h-4 mr-2" />
-						Create Your First Folder
-					</Button>
-				</div>
-			) : (
-				currentDirectory && (
-					<div className="space-y-8 px-4">
-						<DirectoryTable currentDirectory={currentDirectory} />
-						{directoryId !== "root" && (
-							<DiagramTable currentDirectory={currentDirectory} />
-						)}
-					</div>
-				)
-			)}
-		</>
-	);
+  }, [currentDirectoryData, selectItem, directoryUrlId]);
+
+  const createWorkspaceItem = useMutation({
+    mutationFn: async ({
+      type,
+      title,
+    }: {
+      type: "directory" | "diagram" | "form" | "document";
+      title: string;
+    }) => {
+      const response = await fetch("/api/workspace", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type,
+          title,
+          directoryId: currentDirectoryData?.id,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create workspace item");
+      }
+
+      return response.json();
+    },
+    onSuccess: () => {
+      // Invalidate and refetch the current directory data
+      queryClient.invalidateQueries({
+        queryKey: ["directory", directoryUrlId],
+      });
+    },
+  });
+
+  const handleCreateFolder = (
+    type: "diagram" | "directory" | "form" | "document"
+  ) => {
+    createWorkspaceItem.mutate({
+      type,
+      title: `New ${
+        type === "directory"
+          ? "Folder"
+          : type === "diagram"
+          ? "Diagram"
+          : type === "form"
+          ? "Form"
+          : "Document"
+      }`,
+    });
+  };
+
+  const handleGoBack = () => {
+    if (currentDirectoryData?.parent?.directoryId) {
+      const newParams = new URLSearchParams();
+      newParams.set("directoryId", currentDirectoryData.parent.id);
+      router.push(`/dashboard/files?${newParams.toString()}`);
+    } else {
+      router.push("/dashboard/files");
+    }
+  };
+
+  if (isLoading) {
+    return <DashboardSkeleton />;
+  }
+
+  return (
+    <>
+      <ImageOverlayHeader
+        title="Files"
+        icon={<FilesIcon className="size-8 text-primary-foreground z-20" />}
+        leftToolbar={
+          <>
+            {directoryUrlId !== "root" && (
+              <Button
+                variant="secondary"
+                size="icon"
+                onClick={handleGoBack}
+                className="hover:bg-gray-100 dark:hover:bg-mystical/30 z-10"
+              >
+                <ArrowLeft className="w-5 h-5" />
+              </Button>
+            )}
+            <SidebarTrigger className="md:hidden z-10 bg-secondary" />
+          </>
+        }
+        rightToolbar={
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="secondary"
+                className="flex items-center gap-2 dark:bg-accent dark:text-secondary-foreground"
+                disabled={createWorkspaceItem.isPending}
+              >
+                New Item
+                <Plus className="w-5 h-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onClick={() => handleCreateFolder("directory")}
+                className="z-30"
+              >
+                <FaFolder className="w-4 h-4 mr-2" />
+                New Folder
+              </DropdownMenuItem>
+              {directoryUrlId !== "root" && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() => handleCreateFolder("diagram")}
+                  >
+                    <FaProjectDiagram className="w-4 h-4 mr-2" />
+                    New Diagram
+                  </DropdownMenuItem>
+                </>
+              )}
+              {directoryUrlId !== "root" && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={() => handleCreateFolder("form")}>
+                    <FaFileAlt className="w-4 h-4 mr-2" />
+                    New Form
+                  </DropdownMenuItem>
+                </>
+              )}
+              {directoryUrlId !== "root" && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() => handleCreateFolder("document")}
+                  >
+                    <FaFileWord className="w-4 h-4 mr-2" />
+                    New Document
+                  </DropdownMenuItem>
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        }
+      />
+
+      <div className="flex items-center gap-4 py-4 pb-8">
+        <div className="flex items-center space-x-1 justify-between text-muted-foreground p-2 border-b border-border w-full">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/dashboard/files">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              {currentDirectoryData &&
+                currentDirectoryData.title !== "Home" && (
+                  <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>
+                        {currentDirectoryData?.title || "My Workspace"}
+                      </BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </>
+                )}
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div className="flex items-center space-x-2">
+            {currentDirectoryData && currentDirectoryData.title !== "Home" && (
+              <Select
+                value={selectItem ?? undefined}
+                onValueChange={(value) => {
+                  if (value === "remove") {
+                    setSelectItem(null)
+                  }
+                  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+                  setSelectItem(value as any)
+                }}
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Filter Item" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="remove">Remove filter</SelectItem>
+                  <SelectItem value="directory">Folders</SelectItem>
+                  <SelectItem value="diagram">Diagram</SelectItem>
+                  <SelectItem value="form">Form</SelectItem>
+                  <SelectItem value="document">Document</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="p-2 border border-border rounded-lg">
+                  {" "}
+                  <LuLayoutGrid />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mr-6">
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => setViewType("grid")}>
+                  Symbol View
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setViewType("list")}>
+                  List View
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+
+      {filteredFiles?.length === 0 ? (
+        <div className="text-center text-gray-500 py-8">
+          <p className="mb-4">
+            No folders or files yet. Create one to get started!
+          </p>
+          <Button onClick={() => handleCreateFolder("directory")}>
+            <Plus className="w-4 h-4 mr-2" />
+            Create Your First Folder
+          </Button>
+        </div>
+      ) : (
+        filteredFiles && (
+          <div className="space-y-8 px-4">
+            <FilesLayout
+              currentDirectoryData={filteredFiles}
+              viewType={viewType}
+              selectItem={selectItem}
+              directoryUrlId={directoryUrlId}
+            />
+          </div>
+        )
+      )}
+    </>
+  );
 }
---
+++ jinxify/src/components/dashboard/files-table-list.tsx
@@ -0,0 +1,24 @@
+import type { TDirectory } from "@/types/db";
+import FileList from "./files-view/files-list";
+import { Separator } from "../ui/separator";
+
+export function FilesTableList({
+	currentDirectoryData,
+ 
+}: {
+  currentDirectoryData: (TDirectory["documents"][number] | TDirectory["directories"][number] | TDirectory["forms"][number] | TDirectory["diagrams"][number])[];
+  
+}) {
+	return (
+		<div>
+			<h2 className="text-xl font-semibold ">Items</h2>
+			<Separator className="my-4" />
+			<div className="flex flex-col space-y-2">
+            {currentDirectoryData.map((item) => (
+          /* @ts-ignore*/
+          <FileList key={item.id} item={item} />
+        ))}
+			</div>
+		</div>
+	);
+}
